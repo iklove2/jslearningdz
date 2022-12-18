46d1fa87d0803fd98075f08102545d914d804290
iklove2 <iklove@yandex.ru>
2022-12-18 17:49:10 +0300

    feat: сделана домашняя работа №10

---
+++ dz10_2/data.js
@@ -0,0 +1,33 @@
+const dataJson = `[
+
+  { 
+    "name" : "retriever-golden",
+    "img_url": "https://images.dog.ceo/breeds/retriever-golden/n02099601_2359.jpg",
+    "desc": "Очень красивая собака"
+  },
+  {
+    "name" : "shihtzu",
+    "img_url": "https://images.dog.ceo/breeds/shihtzu/n02086240_8825.jpg",
+    "desc": "Невероятно ласковая собака"
+  },
+  {
+    "name" : "rottweiler",
+    "img_url": "https://images.dog.ceo/breeds/rottweiler/n02106550_11762.jpg",
+    "desc": "Кусачая собака"
+  },
+  {
+    "name" : "i don know",
+    "img_url": "https://images.dog.ceo/breeds/springer-english/n02102040_6552.jpg",
+    "desc": "Песель что надо"
+  },
+  {
+    "name" : "i don know",
+    "img_url": "https://images.dog.ceo/breeds/beagle/n02088364_9650.jpg",
+    "desc": "Песель водила"
+  },
+  {
+    "name" : "i don know",
+    "img_url": "https://images.dog.ceo/breeds/ovcharka-caucasian/IMG_20190601_185700.jpg",
+    "desc": "Собакен обалдакен"
+  } 
+]`
