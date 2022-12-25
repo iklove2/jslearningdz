780320b5b54e780dd7e495ff2ae8057647f7e9ef
iklove2 <iklove@yandex.ru>
2022-12-25 14:14:07 +0300

    Big commit

---
+++ cart/data.js
@@ -0,0 +1,27 @@
+const dataProducts = `[
+    { 
+        "item_name" : "Mango people T-SHIRT",
+      "price" : 52,
+      "color": "red",
+      "img_url": "mango_people.png",
+      "size": "x1",
+      "quantity" : 1
+    },
+    { 
+        "item_name" : "ELLERY X M'O CAPSULE",
+      "price" : 70,
+      "color": "green",
+      "img_url": "ellery.jpg",
+      "size": "x1",
+      "quantity" : 2
+    }
+    ,
+    { 
+        "item_name" : "Man & Women",
+      "price" : 89,
+      "color": "purple",
+      "img_url": "others.jpg",
+      "size": "x2",
+      "quantity" : 6
+    }
+  ]`
